1a00125e4d192f5616b2592f3114f3b27be93828
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-04-23 15:39:34 +0200

    Init custom theme

--- Themes/UMPoC.NSMainSiteTheme/Manifest.cs
+++ Themes/UMPoC.NSMainSiteTheme/Manifest.cs
@@ -1,9 +1,9 @@
 using OrchardCore.DisplayManagement.Manifest;
 
 [assembly: Theme(
-    Name = "NS Strona g³ówna",
+    Name = "UMPoC.NSMainSiteTheme",
     Author = "The Orchard Core Team",
     Website = "https://orchardcore.net",
     Version = "0.0.1",
-    Description = "Nowy S¹cz - strona g³ówna"
+    Description = "UMPoC.NSMainSiteTheme"
 )]
