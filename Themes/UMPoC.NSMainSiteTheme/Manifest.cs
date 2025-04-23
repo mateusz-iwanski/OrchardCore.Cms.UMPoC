db9cd6e7072da85fd26cefd38b672de8b0a3a46c
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-04-23 13:25:19 +0200

    Add project files.

---
+++ Themes/UMPoC.NSMainSiteTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "NS Strona g³ówna",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Nowy S¹cz - strona g³ówna"
+)]
